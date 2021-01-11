6b781cf34f4fe0f920db1b98e825e17e109b3e61
soucinthia <sou.cinthia@hotmail.com>
2021-01-11 17:58:05 -0300

    Array

--- js/main.js
+++ js/main.js
@@ -5,7 +5,7 @@ var nome = "Cinthia Souza";
 alert("Bem vindo" + nome);
 */
 
-var nome = "Cinthia Souza";
+/*var nome = "Cinthia Souza";
 var idade = 30;
 //alert(nome + "tem" + idade + "anos");
 var idade2 = 10;
@@ -16,3 +16,14 @@ console.log(idade + idade2);
 console.log(frase.replace("Japao", "Brasil"));
 //alert(frase.replace("Japao", "Brasil"));
 console.log(frase.toUpperCase());
+*/
+
+//Array
+var lista = ["maca", "pera", "laranja"];
+//lista.push("uva"); //acrescentar item na lista
+//lista.pop(); //retira item da lista
+console.log(lista);
+//console.log(lista,length); //tamanho da lista
+console.log(lista.reverse()); //Troca a ordem da lista
+console.log(lista.toString());
+console.log(lista.join(" - "));
