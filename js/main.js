7bc749961eb942879edf9a9d5156e23959baabaa
soucinthia <sou.cinthia@hotmail.com>
2021-01-11 16:53:10 -0300

    Var alert console

--- js/main.js
+++ js/main.js
@@ -1,1 +1,18 @@
+/*
 alert("meu primeiro js");
+
+var nome = "Cinthia Souza";
+alert("Bem vindo" + nome);
+*/
+
+var nome = "Cinthia Souza";
+var idade = 30;
+//alert(nome + "tem" + idade + "anos");
+var idade2 = 10;
+var frase = "Japao e o melhor time do mundo";
+
+console.log(nome);
+console.log(idade + idade2);
+console.log(frase.replace("Japao", "Brasil"));
+//alert(frase.replace("Japao", "Brasil"));
+console.log(frase.toUpperCase());
